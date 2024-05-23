461ae220108e4d6d9e9e5821ac8eb7d51c0c8a2b
Achap05 <chapon.art@gmail.com>
2024-05-23 10:38:50 +0200

    add number_to_string.js and Opposite-number.js

---
+++ 8_Kyu/Opposite-number.js
@@ -0,0 +1,17 @@
+/* Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+function opposite(number) {
+    if(number < 0){
+      return number *= (-1);
+    }
+    else{
+      return number *= (-1);
+    }
+  }
